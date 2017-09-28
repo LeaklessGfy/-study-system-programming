9ff08b18b659876057b95adf151e69133f82608a
Leakless <biokille77@gmail.com>
2017-09-28 18:08:23 +0200

    Better coding convention with clang

--- TP1/src/exo1.c
+++ TP1/src/exo1.c
@@ -1,42 +1,38 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
 
-int main(int argc, char *argv[])
-{
-    struct stat sb;
+int main(int argc, char* argv[]) {
+  struct stat sb;
 
-    if (argc < 2)
-    {
-        fprintf(stderr, "Usage: exo1 <pathname>\n");
-        exit(EXIT_FAILURE);
-    }
+  if (argc < 2) {
+    fprintf(stderr, "Usage: exo1 <pathname>\n");
+    exit(EXIT_FAILURE);
+  }
 
-    if (stat(argv[1], &sb) == -1)
-    {
-        perror("stat");
-        exit(EXIT_FAILURE);
-    }
+  if (stat(argv[1], &sb) == -1) {
+    perror("stat");
+    exit(EXIT_FAILURE);
+  }
 
-    printf("Numéro d'inœud :                   %ld\n", (long) sb.st_ino);
-    printf("Taille du fichier :                %lld octets\n", (long long) sb.st_size);
-    printf("Dernière modification :            %s", ctime(&sb.st_mtime));
-    printf("Type de fichier :                  ");
+  printf("Numéro d'inœud : %ld\n", (long)sb.st_ino);
+  printf("Taille du fichier : %lld octets\n", (long long)sb.st_size);
+  printf("Dernière modification : %s", ctime(&sb.st_mtime));
+  printf("Type de fichier : ");
 
-    switch (sb.st_mode & S_IFMT)
-    {
-        case S_IFDIR:
-            printf("d\n");
-            break;
-        case S_IFREG:
-            printf("f\n");
-            break;
-        default:
-            printf("?\n");
-            break;
-    }
+  switch (sb.st_mode & S_IFMT) {
+    case S_IFDIR:
+      printf("d\n");
+      break;
+    case S_IFREG:
+      printf("f\n");
+      break;
+    default:
+      printf("?\n");
+      break;
+  }
 
-    exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
--- TP1/src/exo4.c
+++ TP1/src/exo4.c
@@ -1,45 +1,37 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
-void get_stat(char *pathname, struct stat *sb)
-{
-    if (stat(pathname, sb) == -1)
-    {
-        perror("stat");
-        exit(EXIT_FAILURE);
-    }
+void get_stat(char *pathname, struct stat *sb) {
+  if (stat(pathname, sb) == -1) {
+    perror("stat");
+    exit(EXIT_FAILURE);
+  }
 }
 
-int main(int argc, char *argv[])
-{
-    time_t t;
-    struct stat sb;
+int main(int argc, char *argv[]) {
+  time_t t;
+  struct stat sb;
 
-    if (argc < 2)
-    {
-        fprintf(stderr, "Usage: exo4 <pathname>\n");
-        exit(EXIT_FAILURE);
-    }
-    
+  if (argc < 2) {
+    fprintf(stderr, "Usage: exo4 <pathname>\n");
+    exit(EXIT_FAILURE);
+  }
+
+  get_stat(argv[1], &sb);
+  t = sb.st_mtime;
+
+  while (1) {
     get_stat(argv[1], &sb);
-    t = sb.st_mtime;
-    
-    while (1)
-    {
-        get_stat(argv[1], &sb);
-
-        if (t != sb.st_mtime)
-        {
-            printf("Change occurd\n");
-            exit(EXIT_SUCCESS);
-        }
-
-        usleep(1000);
+    if (t != sb.st_mtime) {
+      printf("Change occurd on file %s\n", argv[1]);
+      exit(EXIT_SUCCESS);
     }
-    
-    exit(EXIT_SUCCESS);
+    usleep(1000);
+  }
+
+  exit(EXIT_SUCCESS);
 }
